24d69763e1bcf69714e88bdedab5690c001c4275
Tey <teygarciam@hotmail.com>
2018-11-06 23:25:40 -0600

    db

--- API/model/db.js
+++ API/model/db.js
@@ -4,9 +4,9 @@ var mysql = require('mysql');
 
 //local mysql db connection
 var connection = mysql.createConnection({
-    host: 'wootit.cstmcmlaz2y7.us-east-1.rds.amazonaws.com',
-    user: 'wootroot',
-    password: 'n%bDdEPNAYn*',
+    host: '',
+    user: '',
+    password: '',
     database: 'dbo'
 });
 
