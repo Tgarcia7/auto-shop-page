ebd33cd03abaa726f92ecd9860310af627a60b19
Tey García <tgarciamiranda@gmail.com>
2018-11-15 23:17:19 -0600

    layout
    + Layout de gestión de citas
    + Calendario
    + Datatable

---
+++ src/assets/js/gestion.js
@@ -0,0 +1,30 @@
+function gestion(){
+
+  initTableCitas();
+  
+}
+
+function initTableCitas(){
+  $('#citas').DataTable({
+    language: {
+      "decimal": "",
+      "emptyTable": "No hay información",
+      "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+      "infoEmpty": "Mostrando 0 de 0 registros",
+      "infoFiltered": "(Filtrado de _MAX_ registro(s))",
+      "infoPostFix": "",
+      "thousands": ",",
+      "lengthMenu": "Mostrar _MENU_ registros",
+      "loadingRecords": "Cargando...",
+      "processing": "Procesando...",
+      "search": "Buscar:",
+      "zeroRecords": "Sin resultados encontrados",
+      "paginate": {
+          "first": "Primero",
+          "last": "Último",
+          "next": "Siguiente",
+          "previous": "Anterior"
+      }
+    }
+  });
+}
