a6f90d28ddbeecc931270816895ee279fc758036
Tgarcia7 <tgarciamiranda@gmail.com>
2018-11-20 13:48:33 -0600

    Confirmación de citas
    Frontend (ajax)
    + Sweetalerts
    + Aceptar
    + Rechazar

--- src/assets/js/gestion.js
+++ src/assets/js/gestion.js
@@ -81,8 +81,8 @@ function cargarHorarios(){
           if ( estado == '0' ){//cita rechazada
             acciones = '\
                       <div class="btn-group" role="group" aria-label="acciones">\
-                      <button type="button" class="btn btn-default btn-sm" title="Aceptar cita" onClick="aceptar('+idCita+')">\
-                        <span class="hidden-xs">Aceptar</span> <i class="glyphicon glyphicon-ok text-success"></i>\
+                      <button type="button" class="btn btn-default btn-sm" title="Aprobar cita" onClick="aceptar('+idCita+')">\
+                        <span class="hidden-xs">Aprobar</span> <i class="glyphicon glyphicon-ok text-success"></i>\
                       </button>\
                       <button type="button" class="btn btn-default btn-sm" title="Ver detalles de la cita" onClick="detalles('+idCita+')">\
                         <span class="hidden-xs">Detalles</span>  <i class="glyphicon glyphicon-list-alt text-info"></i>\
@@ -95,8 +95,8 @@ function cargarHorarios(){
           if ( estado == '2' ){//cita pendiente
             acciones = '\
                       <div class="btn-group" role="group" aria-label="acciones">\
-                      <button type="button" class="btn btn-default btn-sm" title="Aceptar cita" onClick="aceptar('+idCita+')">\
-                        <span class="hidden-xs">Aceptar</span> <i class="glyphicon glyphicon-ok text-success"></i>\
+                      <button type="button" class="btn btn-default btn-sm" title="Aprobar cita" onClick="aceptar('+idCita+')">\
+                        <span class="hidden-xs">Aprobar</span> <i class="glyphicon glyphicon-ok text-success"></i>\
                       </button>\
                       <button type="button" class="btn btn-default btn-sm" title="Ver detalles de la cita" onClick="detalles('+idCita+')">\
                         <span class="hidden-xs">Detalles</span>  <i class="glyphicon glyphicon-list-alt text-info"></i>\
@@ -122,10 +122,88 @@ function cargarHorarios(){
 
 function aceptar(idCita){
 
+  swal({
+    title: 'Confirmación',
+    text: '¿Desea aprobar la cita seleccionada?',
+    type: 'question',
+    showCancelButton: true,
+    confirmButtonColor: '#5cb85c',
+    confirmButtonText: "Sí, aceptar",
+    cancelButtonText: "No, cancelar",
+  }).then(function(result) {
+    
+    if ( result.value ){
+      $.ajax({
+        type: 'GET',
+        url: "http://localhost:3000/aceptarCita/"+idCita,
+        success:function(resultado){
+          swal({
+            title: '¡Listo!',
+            text: 'La cita ha sido aprobada correctamente.',
+            type: 'success',
+            confirmButtonColor: '#5cb85c',
+            confirmButtonText: 'Aceptar',
+          }).then(function() {
+            cargarHorarios();  
+          });
+        },
+        error:function(resultado){
+          swal({
+            title: 'Error',
+            text: 'Hubo un problema para aprobar la cita, conmuníquese con el encargado del sistema.',
+            type: 'error',
+            confirmButtonColor: '#5cb85c',
+            confirmButtonText: 'Aceptar'
+          });
+        }
+      });
+    }
+    
+  });	
+
 }
 
 function rechazar(idCita){
-  
+
+  swal({
+    title: 'Confirmación',
+    text: '¿Desea rechazar la cita seleccionada?',
+    type: 'question',
+    showCancelButton: true,
+    confirmButtonColor: '#5cb85c',
+    confirmButtonText: "Sí, rechazar",
+    cancelButtonText: "No, cancelar",
+  }).then(function(result) {
+    
+    if ( result.value ){
+      $.ajax({
+        type: 'GET',
+        url: "http://localhost:3000/rechazarCita/"+idCita,
+        success:function(resultado){
+          swal({
+            title: '¡Listo!',
+            text: 'La cita ha sido rechazada correctamente.',
+            type: 'success',
+            confirmButtonColor: '#5cb85c',
+            confirmButtonText: 'Aceptar',
+          }).then(function() {
+            cargarHorarios();  
+          });
+        },
+        error:function(resultado){
+          swal({
+            title: 'Error',
+            text: 'Hubo un problema para rechazar la cita, conmuníquese con el encargado del sistema.',
+            type: 'error',
+            confirmButtonColor: '#5cb85c',
+            confirmButtonText: 'Aceptar'
+          });
+        }
+      });
+    }
+    
+  });	
+
 }
 
 function detalles(idCita){
